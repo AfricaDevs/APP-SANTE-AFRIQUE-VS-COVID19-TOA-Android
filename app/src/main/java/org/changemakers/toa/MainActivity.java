0d2bc4c7206d8773358b45c0421fd3d3d07dd67d
steve111mv <unlivregratuit@gmail.com>
2020-05-17 18:21:59 +0100

    BugFix: drawer toggle strings

--- app/src/main/java/org/changemakers/toa/MainActivity.java
+++ app/src/main/java/org/changemakers/toa/MainActivity.java
@@ -1,10 +1,8 @@
 package org.changemakers.toa;
 
-import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
