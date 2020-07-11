87792672efd5d930b91e0a0f9e11020a93759ceb
steve111mv <unlivregratuit@gmail.com>
2020-07-11 13:20:48 +0100

    Added a new method allowing to get a JSON from Assets resources folder

--- app/src/main/java/com/africadevs/toa/utils/CompatUtils.java
+++ app/src/main/java/com/africadevs/toa/utils/CompatUtils.java
@@ -2,6 +2,9 @@ package com.africadevs.toa.utils;
 
 import android.content.Context;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * 作者 : andy
  * 日期 : 16/1/21 12:07
@@ -12,5 +15,24 @@ public class CompatUtils {
     public static int dp2px(Context context, float dipValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static String getJsonFromAssets(Context context, String fileName) {
+        String jsonString;
+        try {
+            InputStream is = context.getAssets().open(fileName);
+
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            jsonString = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return jsonString;
     }
 }
