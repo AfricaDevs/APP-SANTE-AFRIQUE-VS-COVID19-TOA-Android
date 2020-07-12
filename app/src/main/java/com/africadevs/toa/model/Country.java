a40028ab0d00081b0b853a4bedc75c36b725664f
steve111mv <unlivregratuit@gmail.com>
2020-07-12 09:16:13 +0100

    Implemented a country picker listing countris names from the local Added the countries_list.json from assets directory

--- app/src/main/java/com/africadevs/toa/model/Country.java
+++ app/src/main/java/com/africadevs/toa/model/Country.java
@@ -5,7 +5,8 @@ import android.os.Parcelable;
 
 import java.util.List;
 
-public class Country implements Parcelable {
+public class Country  {
+    public int id;
     public String name;
     public String iso2;
     public String iso3;
@@ -13,44 +14,24 @@ public class Country implements Parcelable {
     public Country() {
 
     }
-    public Country(String name, String iso2, String iso3) {
+
+    public Country(int id, String name, String iso2, String iso3) {
+        this.id = id;
         this.name = name;
         this.iso2 = iso2;
         this.iso3 = iso3;
     }
 
-    protected Country(Parcel in) {
-        name = in.readString();
-        iso2 = in.readString();
-        iso3 = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeString(iso2);
-        dest.writeString(iso3);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Country> CREATOR = new Creator<Country>() {
-        @Override
-        public Country createFromParcel(Parcel in) {
-            return new Country(in);
-        }
-
-        @Override
-        public Country[] newArray(int size) {
-            return new Country[size];
-        }
-    };
-
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setName(String name) {
--- app/src/main/java/com/africadevs/toa/ui/fragments/MainFragment.java
+++ app/src/main/java/com/africadevs/toa/ui/fragments/MainFragment.java
@@ -1,6 +1,7 @@
 package com.africadevs.toa.ui.fragments;
 
 import android.content.Context;
+import android.graphics.Region;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,19 +9,34 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
+import com.africadevs.toa.databinding.FragmentMainBinding;
+import com.africadevs.toa.model.Country;
+import com.africadevs.toa.utils.CompatUtils;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import com.africadevs.toa.MainActivity;
 import com.africadevs.toa.R;
-import com.africadevs.toa.databinding.FragmentMainBinding;
 import com.africadevs.toa.interfaces.MainActivityCallbackInterface;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class MainFragment extends BottomSheetDialogFragment implements View.OnClickListener {
 
     private FragmentMainBinding binding;
 
     private MainActivityCallbackInterface mCallback;
+
+    Country[] countries;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +51,22 @@ public class MainFragment extends BottomSheetDialogFragment implements View.OnCl
 
         binding.cardPrevention.setOnClickListener(this);
         binding.cardDiagnosis.setOnClickListener(this);
+
+        String JSON_STRING =  CompatUtils.getJsonFromAssets(getActivity(), "countries_list.json");
+
+        Gson gson = new Gson();
+
+        countries = gson.fromJson(JSON_STRING, Country[].class);
+
+        String[] displayedCountries = new String[countries.length];
+        for (Country country: countries ) {
+            displayedCountries[country.getId()] = country.getName();
+        }
+
+        binding.countryNumberPicker.setMinValue(0);
+        binding.countryNumberPicker.setMaxValue( countries.length-1 );
+        binding.countryNumberPicker.setDisplayedValues( displayedCountries );
+
 
         return binding.getRoot();
     }
